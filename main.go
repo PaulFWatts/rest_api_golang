e6e5fd8ab46f8a644beefde5b7a3f20f8d164219
Paul Watts <paul.watts@me.com>
2025-07-11 12:07:10 +1000

    Add event creation and retrieval endpoints
    Implemented POST /events and GET /events endpoints in main.go. Added in-memory storage and retrieval for events in models/event.go. Included HTTP test files for creating and retrieving events.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/PaulFWatts/rest_api_golang/models"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
@@ -9,6 +10,7 @@ func main() {
 	server := gin.Default() // Engine instance with default middleware (logger and recovery)
 
 	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
 
 	server.Run(":8080") // Start the server on port 8080
 }
@@ -16,7 +18,25 @@ func main() {
 func getEvents(context *gin.Context) {
 	// Handler logic for retrieving events
 	// This function will be called when a GET request is made to /events
-	context.JSON(http.StatusOK, gin.H{
-		"message": "List of events",
-	})
+	events := models.GetAllEvents()
+	context.JSON(http.StatusOK, events)
+}
+
+func createEvent(context *gin.Context) {
+	// Handler logic for creating a new event
+	// This function will be called when a POST request is made to /events
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+
+	event.Save()
+
+	context.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
 }
--- models/event.go
+++ models/event.go
@@ -3,12 +3,12 @@ package models
 import "time"
 
 type Event struct {
-	ID          string    `json:"id"`
-	Name        string    `json:"name"`
-	Description string    `json:"description"`
-	Location    string    `json:"location"`
-	DateTime    time.Time `json:"datetime"`
-	UserID      int       `json:"user_id"`
+	ID          int
+	Name        string    `binding:"required"`
+	Description string    `binding:"required"`
+	Location    string    `binding:"required"`
+	DateTime    time.Time `binding:"required"`
+	UserID      int
 }
 
 var events = []Event{} // In-memory storage for events
@@ -16,4 +16,8 @@ var events = []Event{} // In-memory storage for events
 func (event Event) Save() {
 	// todo: Implement logic to save the event to a database
 	events = append(events, event)
+}
+
+func GetAllEvents() []Event {
+	return events
 }
