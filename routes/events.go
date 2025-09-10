71c2465820062396d0954bc5f81adb8e3890cf0a
Paul Watts <paul.watts@me.com>
2025-09-10 14:25:10 +1000

    Implement user authorization checks in update and delete event functions

--- routes/events.go
+++ routes/events.go
@@ -168,10 +168,16 @@ func updateEvent(context *gin.Context) {
 		return
 	}
 
-	_, err = models.GetEventByID(eventId)
+	userID := context.GetInt64("userId")
+	event, err := models.GetEventByID(eventId)
 
 	if err != nil {
 		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch the event."})
+		return
+	}
+
+	if event.UserID != userID {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized to update this event."})
 		return
 	}
 
@@ -229,11 +235,17 @@ func deleteEvent(context *gin.Context) {
 		return
 	}
 
+	userID := context.GetInt64("userId")
 	event, err := models.GetEventByID(eventId)
 
 	if err != nil {
 		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch the event."})
 		return
+	}
+
+	if event.UserID != userID {
+	context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized to delete this event."})
+	return
 	}
 
 	err = event.Delete()
